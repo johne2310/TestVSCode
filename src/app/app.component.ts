3d3f6215f8634be26ab9253cca1ad5ab7f9ed122
John Evans <John Evans>
2017-01-01 13:08:29 +1100

    Test Commit 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor() {
+    let obs = Observable
+      .interval(1000)
+      .take(5)
+      .map(a => a * 4);
+
+    obs
+      .subscribe(data => console.log(`Subscriber: ${data}`));
+  }
+
+}
